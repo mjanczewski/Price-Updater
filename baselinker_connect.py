b357a416d213c0042634ea158255b64f6b8f8c58
Marek Janczewski <mrjanczewski@gmail.com>
2024-03-19 15:06:20 +0100

    Dodanie iteracji po zamówieniach, BL maksymalnie pozwala na pobieranie 100

--- baselinker_connect.py
+++ baselinker_connect.py
@@ -22,12 +22,26 @@ class BaselinkerConnect:
         # TODO pobranie ostatniego zamówienia za pomoca metody get_last_order_id
         last_order = database_connect.DataBaseConnect()
         last_order_id = last_order.get_last_order_id()
-        print(last_order_id)
-        data = {"token": self.TOKEN, "method": self.get_orders }
 
-        baselinker_response = requests.post(self.URL, data=data)
+        data = {"token": self.TOKEN, "method": self.get_orders}
 
-        print(baselinker_response.json())
+        baselinker_response = requests.post(self.URL, data=data).json()
+
+        for i in range(len(baselinker_response["orders"])):
+            number_of_products = len(baselinker_response["orders"][i]["products"])
+            last_order_in_baselinker = baselinker_response["orders"][i]["orders_id"]
+            for j in range(number_of_products):
+                if baselinker_response["orders"][i]["admin_comments"] != "":
+                    bl_order_id = baselinker_response["orders"][i]["order_id"]
+                    bl_admin_comment = baselinker_response["orders"][i]["admin_comments"]
+                    bl_user_login = baselinker_response["orders"][i]["user_login"]
+                    bl_user_fullname = baselinker_response["orders"][i]["delivery_fullname"]
+                    bl_sku = baselinker_response["orders"][i]["products"][j]["sku"]
 
 
-abc = BaselinkerConnect()
+    def get_invoices_number_from_baselinker(self):
+        pass
+
+
+get_orders_info = BaselinkerConnect()
+get_orders_info.get_orders_from_baselinker()
